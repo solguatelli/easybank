2a820ad5847de38be41d6891d7abd93cec12942b
solguatelli <sol.guatelli@gmail.com>
2021-01-18 21:27:58 -0300

    First commit

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body")
+
+
+hamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+  
+  if (header.classList.contains("open")) { // Close hamburger menu
+    header.classList.remove("open")
+    body.classList.remove("no-scroll")
+
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in")
+      element.classList.add("fade-out")
+    })
+    
+  } else {                                //Open hamburger menu
+    header.classList.add("open")
+    body.classList.add("no-scroll")
+
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out")
+      element.classList.add("fade-in")
+    })
+  }
+   
+});
